5dd569611a01820d65919f7cf91e738ad3b57f38
Carlo Del Don <deldoc@usi.ch>
2026-03-17 16:57:36 +0100

    check if pre-commit fails

--- entrypoints/serve_prefect_maintenance.py
+++ entrypoints/serve_prefect_maintenance.py
@@ -7,6 +7,9 @@ from irsol_data_pipeline.orchestration.flows.delete_old_prefect_data import (
 
 def main():
 
+
+
+
     delete_old_prefect_data_deployment = delete_flow_runs_older_than.to_deployment(
         name="delete-old-prefect-flow-runs",
         parameters={"hours": 24 * 7 * 4, "interactive": False},
