c85d19d70df2fb8c576e19ddfd097b14b64f2a3b
Carlo Del Don <deldoc@usi.ch>
2026-03-30 10:23:01 +0200

    fix: suppress SunpyUserWarning to reduce verbosity in warnings

--- src/irsol_data_pipeline/__init__.py
+++ src/irsol_data_pipeline/__init__.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import os
+import warnings
 
 from .version import __version__
 
@@ -22,5 +23,11 @@ if "MPLBACKEND" not in os.environ and not has_display():
 
     matplotlib.use("Agg")
 
+# Suppress warnings from sunpy, which can be verbose and not relevant to users of this package.
+# The reason we get this type of warnings, is that sunpy is installed without some "optional" packages such as `mlp-animators` etc.
+# For the purpose of this library, these extra features are not needed
+from sunpy.util import SunpyUserWarning as _SunpyUserWarning  # noqa
+
+warnings.filterwarnings("ignore", category=_SunpyUserWarning, module="sunpy")
 
 __all__ = ["__version__", "has_display"]
